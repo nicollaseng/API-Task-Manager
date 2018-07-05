5ba17c6e58286297d337f13c3d5b9fdd67fae7d1
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-07-05 10:33:35 -0300

    Setting route namespace api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    namespace :api, defaults: { format: :json }, constrains: {subdomain: 'api'}, path: "/" do 
-      
+    namespace :api, defaults: { format: :json }, 
+    			constraints: {subdomain: 'api'}, path: "/" do 
+
+
     end
 
 end
