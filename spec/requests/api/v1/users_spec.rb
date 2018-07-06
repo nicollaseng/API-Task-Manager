52c2ee7375d393f2225d4e5946fa0475be06d435
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-07-06 00:23:10 -0300

    More tests implemented

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe 'Users API', type: :request do
 	let!(:user) {FactoryBot.create(:user)}
 	let(:user_id) {user.id}
 
+	before {host! "api.taskmanager.test"}
+
 	describe "GET /users/:id" do 
 		before do
 			headers = {"Accept" => "application/vnd.taskmanager.v1"}
-			get "/users/#{user_id}", {}, headers
+			get "/users/#{user_id}", params: {}, headers: headers
 		end
 
 		context "when user exists" do 
@@ -15,6 +17,17 @@ RSpec.describe 'Users API', type: :request do
 				user_response = JSON.parse(response.body)
 				expect(user_response["id"]).to eq(user_id)
 			end
+
+			it 'returns status 200' do 
+				expect(response).to have_http_status(200)
+			end
 		end
-	end
+
+		context 'when user does not exists' do
+			let(:user_id) {10000}
+			it 'returns status code 404' do 
+				expect(response).to have_http_status(404)
+			end
+		end
+ 	end
 end
